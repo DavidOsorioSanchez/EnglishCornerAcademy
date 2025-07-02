6349dc102ead37573cf60b4c59cbd5bf99a14460
David Alejandro Osorio Sánchez <david.osorio004@mymdc.net>
2025-07-01 23:40:00 -0500

    terminando la ResponsiveToolBar

--- src/components/UI/ListItem.tsx
+++ src/components/UI/ListItem.tsx
@@ -10,7 +10,7 @@ export default function ListItem({
     return (
         <li {...props}>
             <NavigationMenuLink asChild>
-                <a href={`#${href}`} className="scroll-smooth">
+                <a href={`#${href}`} className="h-16 flex flex-col gap-0 items-start justify-center ">
                     <div className="text-sm leading-none font-medium">{title}</div>
                     <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
                         {children}
